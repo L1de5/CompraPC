408fc80289889a16ad39e764742be553ef25241a
Rodrigo Martins <rodrigo.martins7121@gmail.com>
2019-10-16 10:23:43 -0300

    arrumando problema de adicionar imagens aos produtos

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 from app.ext import database, login
 
 app = Flask('app')
-app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['UPLOAD_FOLDER'] = 'app/static/uploads'
 app.config.from_pyfile('../config.cfg')
 database.configure(app)
 login.configure(app)
--- app/controllers/produto_bp.py
+++ app/controllers/produto_bp.py
@@ -12,20 +12,20 @@ from flask_login import login_required
 from werkzeug.utils import secure_filename
 from hashlib import md5
 from sqlalchemy import exc
+from uuid import uuid4
 
 produtos_bp = Blueprint('produtos', __name__, url_prefix='/produto')
 
-def permitido(name): 
-    return'.'in name and name.split('.')[-1].lower() in ['png', 'jpg', 'jpeg']
+def permitido(extencao): 
+    return extencao in ['png', 'jpg', 'jpeg']
 
 @produtos_bp.route('/')
 def listar():
     form_cadastro = CadastroForm()
     form_login = LoginForm()
     form_add_produto = ProdutoForm()
-
     produtos = Produto.query.all()
-    print(produtos)
+
     return render_template('buscas/listarp.html', produtos = produtos, form_cadastro = form_cadastro, form_login = form_login, form_add_produto = form_add_produto)
 
 @produtos_bp.route('/adicionar', methods=['GET', 'POST'])
@@ -39,23 +39,32 @@ def produto():
         preco = form.preco.data
         quantidade = form.quantidade.data
         arquivo = form.arquivo.data
-
-        if arquivo and permitido(arquivo.filename):
+        extencao_foto = arquivo.filename.split('.')[-1].lower()
+        novo_nome_foto = uuid4()
+        is_permitido = permitido(extencao_foto)
+         
+        if is_permitido:
+            novo_nome_foto = str(novo_nome_foto) + '.' + extencao_foto
             foto = secure_filename(arquivo.filename)
-            arquivo.save(os.path.join('app', app.config['UPLOAD_FOLDER'], foto))
-            diretorio = os.path.join('app', app.config['UPLOAD_FOLDER'], foto)
-            produto = Produto(nome = nome, descricao = descricao, preco = preco, quantidade = quantidade, arquivo = diretorio)
+            diretorio = os.path.join(app.config['UPLOAD_FOLDER'], foto)
+            diretorio_novo = os.path.join(app.config['UPLOAD_FOLDER'], novo_nome_foto)
 
-        try:
-            db.session.add(produto)
-            db.session.commit()
-         #login_usuario(new_user) Explica ai
+            arquivo.save(diretorio)
+            os.rename(diretorio, diretorio_novo)
 
-            return redirect('/produto')
-        except exc.SQLAlchemyError:
-            flash(u'Ocorreu um problema ao tentar cadastrar usuário, tente novamente!', 'danger')
+            produto = Produto(nome = nome, descricao = descricao, preco = preco, quantidade = quantidade, arquivo = 'uploads/'+novo_nome_foto)
 
-            return render_template('/produto')
+            try:
+                db.session.add(produto)
+                db.session.commit()
+
+                return redirect('/produto')
+            except exc.SQLAlchemyError:
+                flash(u'Ocorreu um problema ao tentar adicionar produto, tente novamente!', 'danger')
+
+                return render_template('/produto')
+        else:
+            flash(u'Ocorreu um problema ao tentar adicionar produto, tente novamente!', 'danger')
 
     return render_template('index.html', form=form, titulo='Produto')
 
