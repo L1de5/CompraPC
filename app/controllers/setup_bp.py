4e9a040b032727bc7daec1df39f421b6b2f4b9c2
Rodrigo Martins <rodrigo.martins7121@gmail.com>
2019-11-19 14:15:24 -0300

    Adicionando rota de popular banco

--- app/controllers/setup_bp.py
+++ app/controllers/setup_bp.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint
+from flask import Blueprint, flash, redirect
 from app import app
 from app.ext.database import db
 from sqlalchemy import exc
 from app.models.banco.Usuario import Usuario
+from hashlib import md5
 
 setup_bp = Blueprint('setup', __name__, url_prefix='/setup')
 
@@ -35,3 +36,17 @@ def drop():
         return 'Tabelas excluidas com sucesso <br> ' + link_home_setup
     except exc.SQLAlchemyError:
         return 'Falha ao excluir tableas <br> ' + link_home_setup
+
+@setup_bp.route('/popula_banco')
+def popula_banco():
+    adm = Usuario(nome = 'Cleber', email = 'admin', senha = senha.hexdigest(), endereco = endereco, cpf = cpf, data_nasc = data_nasc)
+    
+    try:
+
+        db.session.add(produto)
+        db.session.commit()
+        flash(u'Banco de dados populado com sucesso <br> ADM Email: adm@email.com Senha: adm123', 'danger')
+
+        return redirect('/produto')
+    except exc.SQLAlchemyError:
+        return 'Falha ao popular tableas <br> ' + link_home_setup
